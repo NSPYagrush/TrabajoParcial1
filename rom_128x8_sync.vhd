40d7ae2fc5c443884de687bac2e265accde4c825
NSPYagrush <79429719+NSPYagrush@users.noreply.github.com>
2021-03-31 11:41:48 -0500

    Update rom_128x8_sync.vhd

--- rom_128x8_sync.vhd
+++ rom_128x8_sync.vhd
@@ -4,9 +4,9 @@ use IEEE.numeric_std.all;
 
 entity rom_128x8_sync is
     port (
-		  clock 		: in std_logic;
-        address	: in std_logic_vector (7 downto 0);
-		  data_Out  : out std_logic_vector (7 downto 0));
+	clock	  : in std_logic;
+        address	  : in std_logic_vector (7 downto 0);
+	data_Out  : out std_logic_vector (7 downto 0));
 end entity;
 
 architecture rom_128x8_sync_arch of rom_128x8_sync is
@@ -18,19 +18,20 @@ architecture rom_128x8_sync_arch of rom_128x8_sync is
 	constant STA_DIR : std_logic_vector (7 downto 0) := x"96";
 	constant STB_DIR : std_logic_vector (7 downto 0) := x"97";
 	constant ADD_AB  : std_logic_vector (7 downto 0) := x"42";
-	constant BRA 	  : std_logic_vector (7 downto 0) := x"20";
-	constant BEQ 	  : std_logic_vector (7 downto 0) := x"23";
+	constant BRA 	 : std_logic_vector (7 downto 0) := x"20";
+	constant BEQ 	 : std_logic_vector (7 downto 0) := x"23";
 	
 	type rom_type is array (0 to 127) of std_logic_vector(7 downto 0);
 	
-	constant ROM : rom_type := (0 => LDA_IMM,
-										 1 => x"AA",
-										 2 => STA_DIR,
-										 3 => x"E0",
-										 4 => BRA,
-										 5 => x"00",
-								  others => x"00");
-										 
+	constant ROM : rom_type := (
+					0 => LDA_IMM,
+				 	1 => x"AA",
+					2 => STA_DIR,
+					3 => x"E0",
+					4 => BRA,
+					5 => x"00",
+					others => x"00");
+						 
 	signal EN : std_logic;
 										 
 	begin
