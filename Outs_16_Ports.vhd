1ce49b765ee46e8a485dc66cad059957c0f3812d
NSPYagrush <79429719+NSPYagrush@users.noreply.github.com>
2021-03-31 11:31:24 -0500

    Update Outs_16_Ports.vhd

--- Outs_16_Ports.vhd
+++ Outs_16_Ports.vhd
@@ -3,16 +3,16 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity Outs_16_Ports is
-    port (
-        reset 		: in std_logic;
-		  clock 		: in std_logic;
-		  write 		: in std_logic;
-		  address 	: in std_logic_vector (7 downto 0);
-		  data_in 	: in std_logic_vector (7 downto 0);
-		  port_Out_00, port_Out_01, port_Out_02, port_Out_03, 
-		  port_Out_04, port_Out_05, port_Out_06, port_Out_07,
-		  port_Out_08, port_Out_09, port_Out_10, port_Out_11, 
-		  port_Out_12, port_Out_13, port_Out_14, port_Out_15 	: out std_logic_vector (7 downto 0));
+	port (
+        	reset	: in std_logic;
+		clock 	: in std_logic;
+		write 	: in std_logic;
+		address : in std_logic_vector (7 downto 0);
+		data_in : in std_logic_vector (7 downto 0);
+		port_Out_00, port_Out_01, port_Out_02, port_Out_03, 
+		port_Out_04, port_Out_05, port_Out_06, port_Out_07,
+		port_Out_08, port_Out_09, port_Out_10, port_Out_11, 
+		port_Out_12, port_Out_13, port_Out_14, port_Out_15 : out std_logic_vector (7 downto 0));
 end entity;
 
 architecture Outs_16_Ports_arch of Outs_16_Ports is
