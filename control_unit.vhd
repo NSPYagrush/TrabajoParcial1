2e4d78d81b6db04e5092feeb31592fdcb93354e5
NSPYagrush <79429719+NSPYagrush@users.noreply.github.com>
2021-03-31 11:29:47 -0500

    Update control_unit.vhd

--- control_unit.vhd
+++ control_unit.vhd
@@ -4,21 +4,21 @@ use IEEE.numeric_std.all;
 
 entity control_unit is 
 	port(
-			Clock 		: in std_logic;
-			Reset 		: in std_logic;
-			CCR_Result  	: in std_logic_vector(3 downto 0);
-			IR 		: in std_logic_vector(7 downto 0);
-			IR_Load 	: out std_logic;
-			MAR_Load 	: out std_logic;
-			PC_Load 	: out std_logic;
-			PC_Inc 		: out std_logic;
-			A_Load 		: out std_logic;
-			B_Load 		: out std_logic;
-			CCR_Load 	: out std_logic;
-			ALU_Sel 	: out std_logic_vector(2 downto 0);
-			Bus2_Sel 	: out std_logic_vector(1 downto 0);
-			Bus1_Sel 	: out std_logic_vector(1 downto 0);
-			write 		: out std_logic);
+		Clock 		: in std_logic;
+		Reset 		: in std_logic;
+		CCR_Result  	: in std_logic_vector(3 downto 0);
+		IR 		: in std_logic_vector(7 downto 0);
+		IR_Load 	: out std_logic;
+		MAR_Load 	: out std_logic;
+		PC_Load 	: out std_logic;
+		PC_Inc 		: out std_logic;
+		A_Load 		: out std_logic;
+		B_Load 		: out std_logic;
+		CCR_Load 	: out std_logic;
+		ALU_Sel 	: out std_logic_vector(2 downto 0);
+		Bus2_Sel 	: out std_logic_vector(1 downto 0);
+		Bus1_Sel 	: out std_logic_vector(1 downto 0);
+		write 		: out std_logic);
 end entity;
 
 architecture control_unit_arch of control_unit is
